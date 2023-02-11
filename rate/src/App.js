ccdf90a1705d71dc0b9a77e680096956feaa9623
Sasha Sasha <sashamaninaa@mail.ru>
2023-02-11 23:00:41 +0300

    исправила баг

--- rate/src/App.js
+++ rate/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import './App.css';
+import './components/CardApp.scss';
 import cards from './utils/cards';
 import Card from './components/Card';
 
 class App extends React.Component {
   render() {
     return (
-      <>{
+      <div className='CardApp'>{
         cards.map((card) =>
           <Card key={card.id} {...card} />
 
         )
       }
-      </>
+      </div>
 
     );
   }
